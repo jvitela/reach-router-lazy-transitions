8bcd38f200ae273f2f61f7853b50beeae575a97b
jvitela <Jonathan Vitela jonathan.vitela@2e-systems.com>
2019-06-04 12:31:26 +0200

    GH Pages first commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# reach-router-lazy-transitions
-Example of Reach-Router + React.lazy + async getInitialProps + CSSTransition
---
+++ precache-manifest.b32d0d5f52465f4527767c66e5699d90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "404c96be0865668c545c68bd6764533a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2d5dde8a05e698c92a5",
+    "url": "/static/css/main.7f93aa0b.chunk.css"
+  },
+  {
+    "revision": "f7d9f5f3ea52a1413cf2",
+    "url": "/static/js/2.8b5f77c1.chunk.js"
+  },
+  {
+    "revision": "efe972a6bf2725f51b6f",
+    "url": "/static/js/3.78da43ab.chunk.js"
+  },
+  {
+    "revision": "b8a38123c71a7b54ebbe",
+    "url": "/static/js/4.76002ef6.chunk.js"
+  },
+  {
+    "revision": "b2d5dde8a05e698c92a5",
+    "url": "/static/js/main.1ae8bbeb.chunk.js"
+  },
+  {
+    "revision": "865547851e8ad56ba9a7",
+    "url": "/static/js/runtime~main.2a1b35a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
